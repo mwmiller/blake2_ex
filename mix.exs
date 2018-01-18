eadc4d917dd15e119b7a43b28c32e1b655df1ab3
Matt Miller <mwm@fastmail.com>
2018-01-18 23:04:28 +0200

    Apply Elixir 1.6 mix format

--- mix.exs
+++ mix.exs
@@ -2,16 +2,18 @@ defmodule Blake2.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :blake2,
-     version: "1.0.1",
-     elixir: "~> 1.4",
-     name: "Blake2",
-     source_url: "https://github.com/mwmiller/blake2_ex",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: description(),
-     package: package(),
-     deps: deps()]
+    [
+      app: :blake2,
+      version: "1.0.1",
+      elixir: "~> 1.4",
+      name: "Blake2",
+      source_url: "https://github.com/mwmiller/blake2_ex",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      deps: deps()
+    ]
   end
 
   def application do
@@ -22,7 +24,7 @@ defmodule Blake2.Mixfile do
     [
       {:earmark, "~> 1.0", only: :dev},
       {:ex_doc, "~> 0.14", only: :dev},
-      {:credo, "~> 0.7", only: [:dev, :test]},
+      {:credo, "~> 0.7", only: [:dev, :test]}
     ]
   end
 
@@ -34,13 +36,13 @@ defmodule Blake2.Mixfile do
 
   defp package do
     [
-     files: ["lib", "mix.exs", "README*", "LICENSE*", ],
-     maintainers: ["Matt Miller"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/mwmiller/blake2_ex",
-              "RFC"    => "https://tools.ietf.org/html/rfc7693",
-             }
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Matt Miller"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/mwmiller/blake2_ex",
+        "RFC" => "https://tools.ietf.org/html/rfc7693"
+      }
     ]
   end
-
 end
--- test/blake2_test.exs
+++ test/blake2_test.exs
@@ -5,54 +5,57 @@ defmodule Blake2Test do
 
   test "RFC 2b example" do
     m = "abc"
-    h = from_hex """
-                  BA 80 A5 3F 98 1C 4D 0D 6A 27 97 B6 9F 12 F6 E9
-                  4C 21 2F 14 68 5A C4 B7 4B 12 BB 6F DB FF A2 D1
-                  7D 87 C5 39 2A AB 79 2D C2 52 D5 DE 45 33 CC 95
-                  18 D3 8A A8 DB F1 92 5A B9 23 86 ED D4 00 99 23
-                 """
+
+    h =
+      from_hex("""
+       BA 80 A5 3F 98 1C 4D 0D 6A 27 97 B6 9F 12 F6 E9
+       4C 21 2F 14 68 5A C4 B7 4B 12 BB 6F DB FF A2 D1
+       7D 87 C5 39 2A AB 79 2D C2 52 D5 DE 45 33 CC 95
+       18 D3 8A A8 DB F1 92 5A B9 23 86 ED D4 00 99 23
+      """)
 
     assert Blake2.hash2b(m) == h
-
   end
 
   test "RFC 2s example" do
     m = "abc"
-    h = from_hex """
-                  50 8C 5E 8C 32 7C 14 E2 E1 A7 2B A3 4E EB 45 2F
-                  37 45 8B 20 9E D6 3A 29 4D 99 9B 4C 86 67 59 82
-                 """
+
+    h =
+      from_hex("""
+       50 8C 5E 8C 32 7C 14 E2 E1 A7 2B A3 4E EB 45 2F
+       37 45 8B 20 9E D6 3A 29 4D 99 9B 4C 86 67 59 82
+      """)
 
     assert Blake2.hash2s(m) == h
-
   end
 
   test "repo 2b test vectors" do
-    k = RepoVectors.key2b
+    k = RepoVectors.key2b()
 
     test_2b = fn
-              ([],[], _fun)              -> :noop
-              ([m|ins], [h|hashes], fun) ->
-                assert (Blake2.hash2b(m,64,k) |> tag_from_bin) == h
-                fun.(ins, hashes, fun)
-              end
+      [], [], _fun ->
+        :noop
 
-    test_2b.(RepoVectors.ins, RepoVectors.hashes2b, test_2b)
+      [m | ins], [h | hashes], fun ->
+        assert Blake2.hash2b(m, 64, k) |> tag_from_bin == h
+        fun.(ins, hashes, fun)
+    end
 
+    test_2b.(RepoVectors.ins(), RepoVectors.hashes2b(), test_2b)
   end
 
   test "repo 2s test vectors" do
-    k = RepoVectors.key2s
+    k = RepoVectors.key2s()
 
     test_2s = fn
-              ([],[], _fun)              -> :noop
-              ([m|ins], [h|hashes], fun) ->
-                assert (Blake2.hash2s(m,32,k) |> tag_from_bin) == h
-                fun.(ins, hashes, fun)
-              end
+      [], [], _fun ->
+        :noop
 
-    test_2s.(RepoVectors.ins, RepoVectors.hashes2s, test_2s)
+      [m | ins], [h | hashes], fun ->
+        assert Blake2.hash2s(m, 32, k) |> tag_from_bin == h
+        fun.(ins, hashes, fun)
+    end
 
+    test_2s.(RepoVectors.ins(), RepoVectors.hashes2s(), test_2s)
   end
-
 end
